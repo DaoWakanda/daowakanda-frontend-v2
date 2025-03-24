c54c22e831f373fdff675561a346143b8600aba6
delyte <madunecheezechukwu@gmail.com>
2025-03-24 13:32:03 +0100

    updated file structure

--- src/features/events/naija-make-your-move/index.tsx
+++ src/features/events/naija-make-your-move/index.tsx
@@ -1,7 +1,7 @@
 import BenefitSection from "./hackathon-component/benefit-section"
 import HeroSection from "./hackathon-component/hero-section"
 import MainSection from "./hackathon-component/main-content-section"
-import PriceSection from "./hackathon-component/Price/PriceSection"
+import PriceSection from "./hackathon-component/price"
 
 const NaijaMakeYourMove = () => {
   return (
