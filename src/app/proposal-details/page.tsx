97265321fdb2550e75bb2c9f3e369c18eb2ae22d
Adeyemi Sadiq <adexsadiq01@gmail.com>
2025-04-17 22:34:51 +0100

    proposal detail page and submit modal

---
+++ src/app/proposal-details/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProposalDetailsPage from '@/features/proposal-details-page';
+
+const ProposalDetails = () => {
+  return <ProposalDetailsPage />;
+};
+
+export default ProposalDetails;
