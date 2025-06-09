032de6a266021bcd46bb64463203fe899f98c151
sylvamadu <54332482+sylvamadu@users.noreply.github.com>
2025-06-09 14:54:34 +0100

    Auth and Profile Flow

--- src/features/dev-challenges-details-page/access-denied/index.tsx
+++ src/features/dev-challenges-details-page/access-denied/index.tsx
@@ -1,7 +1,15 @@
+import { Button } from '@/components/button';
+import { ConnectWalletVisibleAtom } from '@/state';
 import Link from 'next/link';
 import React from 'react';
+import { useRecoilState } from 'recoil';
 
-const AccessDeniedPage = () => {
+interface Props {
+  isConnected: boolean;
+}
+const AccessDeniedPage = ({ isConnected }: Props) => {
+  const [showWalletConnect, setShowWalletConnect] = useRecoilState(ConnectWalletVisibleAtom);
+
   return (
     <div className="bg-[#1C1B1C] w-full min-h-screen rounded-[16px] p-6 md:p-10 ">
       <div className="w-[194px] aspect-square md:w-[349px] mx-auto my-8">
@@ -13,11 +21,19 @@ const AccessDeniedPage = () => {
       <div className="flex flex-col items-center justify-center text-center mt-4 space-y-3">
         <h2 className="font-bold text-[32px]">Access denied</h2>
         <p className="text-[16px] md:text-[22px]">
-          Access to this task has been denied, update your profile to gain access.
+          {isConnected
+            ? ` Access to this task has been denied, update your profile to gain access.`
+            : ` Access to this task has been denied, connect your wallet to gain access.`}
         </p>
-        <Link className="text-[#C7F284] text-[16px]" href="/developers/signup">
-          Proceed to update KYC
-        </Link>
+        {isConnected ? (
+          <Link className="text-[#C7F284] text-[16px]" href="/developers/signup">
+            Proceed to update KYC
+          </Link>
+        ) : (
+          <Button className="text-[#C7F284] text-[16px]"  onClick={() => setShowWalletConnect(true)}>
+            Connect wallet
+          </Button>
+        )}
       </div>
     </div>
   );
